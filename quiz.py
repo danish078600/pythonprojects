4b61684daa25b7f62f448a9251d15d629239e2e5
danish078600 <mddanishalauddin@gmail.com>
2023-10-14 18:30:23 +0530

    #quiz initial

--- quiz.py
+++ quiz.py
@@ -1,1 +1,15 @@
-print("hello")
+#--------------
+def new_game():
+    pass
+
+#---------------
+def check_answer():
+    pass
+
+#---------------
+def display_score():
+    pass
+
+#---------------
+def play_again():
+    pass
